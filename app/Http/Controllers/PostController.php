d0bbbb9114bd86a82f6bda09f4e14a86956fda05
muhammad abdelhay <abdelhay.courses@gmail.com>
2021-03-21 14:18:44 +0200

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class PostController extends Controller {
+    public function index(){
+        $posts = Post::all();
+        //$posts = Post::paginate(15);
+        return view('posts.index', compact('posts'));
+    }
+
+    public function show(Post $post){
+        return view('posts.show', compact('post'));
+    }
+
+    public function create(){
+        $users = User::all();
+        return view('posts.create', compact('users'));
+    }
+
+    public function store(Request $request){
+        Post::create($request->all());
+        return redirect()->route('posts.index');
+    }
+
+    public function edit(Post $post){
+        $users = User::all();
+        return view('posts.edit', compact('post', 'users'));
+    }
+
+    public function update(Post $post, Request $request){
+        $post->update($request->all());
+        return redirect()->route('posts.index');
+    }
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect()->route('posts.index');
+    }
+
+}
